5dc129709aa269a1f0e046d091d7f9ee89352b72
Amanuel <amanuelsolomon@gmail.com>
2022-08-23 11:50:09 -0700

    adjfa

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-for x in range(97,123):
+for x in range(97, 123):
     if x != 101 and x != 113:
-        print("{}".format(chr(i)), end = "")
+        print("{}".format(chr(i)), end="")
