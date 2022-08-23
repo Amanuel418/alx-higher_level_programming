ab0de529ba076aa9f442caa5702fc1ca864d72be
Amanuel <amanuelsolomon@gmail.com>
2022-08-23 11:15:26 -0700

    adlfa

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -5,8 +5,8 @@ num = str(number)
 num = str[-1]
 num = int(num)
 if num > 5:
-    return "Last digit of {} is {} and is greater than 5".format(number,num)
+    print("Last digit of {} is {} and is greater than 5".format(number,num))
 elif num == 0:
-    return "Last digit of {} is {} and is 0".format(number,num)
+    print("Last digit of {} is {} and is 0".format(number,num))
 else:
-    return "Last digit of {} is {} and is less than 6 and not 0".format(number,num)
+    print("Last digit of {} is {} and is less than 6 and not 0".format(number,num))
